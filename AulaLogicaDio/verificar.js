3cc7c4b84ba8051e3324d6707c7cbfc608f65791
JonathasGit <jonathas-25@hotmail.com>
2026-02-12 22:57:30 -0300

    SUbindo um JS

---
+++ AulaLogicaDio/verificar.js
@@ -0,0 +1,9 @@
+let numero ="1" // Somente um = é uma atribuição
+console.log (numero === 1) //Compara o valor e o formato do conteudo 
+console.log (numero == 1 ) // compara valor 
+
+
+//!== compara o que é diferente, exemplo abaixo 
+
+let marca = "Apple" 
+console.log (marca !== "Samsung") // retornar true
